fe71069596cf7c890dcd2a445940eec7fad27c90
hyndf05 <hyndf05@gmail.com>
2022-11-15 14:47:50 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>WiSSLX</title>
+    <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="/d/css/main.css" />
+    <link rel="stylesheet" href="/d/css/root.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script defer src=""></script>
+  </head>
+  <body>
+    <div class="wrap">
+      <div class="header">
+        <div class="mhdr ptre pdlr background mxw wauto">
+          <a href="/" class="pos">
+            <div class="logo">
+              <span>WiSSLX</span>
+            </div>
+          </a>
+          <div class="search">
+            <div class="background wauto">
+              <div class="ptre pldir wauto">
+                <input type="search" class="search-ent">
+                <button class="search-ent">
+                  <img src="https://uploads.hyndf05.repl.co/wisslx/icons/search.svg">
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="shdr">
+          <ul class="pList mxw">
+            <li class="rBlocks dpib">
+              <a href="/mail">메일</a>
+            </li>
+            <li class="rBlocks dpib">
+              <a href="/log">로그</a>
+            </li>
+            <li class="rBlocks dpib">
+              <a href="/news">뉴스</a>
+            </li>
+            <li class="rBlocks dpib">
+              <a href="/community">커뮤니티</a>
+            </li>
+            <li class="rBlocks dpib">
+              <a href="/shopping">쇼핑</a>
+            </li>
+            <li class="rBlocks dpib">
+              <a href="/wiki">위키</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="content mxw">
+        <div class="mContent">
+          <div class="cim">
+            <div class="wc-advertising wc-bb-advertising">
+              <span>AD</span>
+            </div>
+            <div class="wc-cover"></div>
+          </div>
+        </div>
+        <div class="sContent">
+          <div class="cim">
+            <div class="wc-profile">
+              <div class="wc-p-login wc-block">
+                <div class="wc-title">
+                  <span>로그인</span>
+                </div>
+                <div class="wc-p-login-methods">
+                  <a href="/auth/login?with=naver">
+                    <div class="wc-p-login-methods-item naver">
+                      <span>네이버로 로그인</span>
+                    </div>
+                  </a>
+                  <a href="/auth/login?with=kakao">
+                    <div class="wc-p-login-methods-item kakao">
+                      <span>카카오로 로그인</span>
+                    </div>
+                  </a>
+                  <a href="/auth/login?with=wisslx">
+                    <div class="wc-p-login-methods-item wisslx">
+                      <span>WISSLX로 로그인</span>
+                    </div>
+                  </a>
+                </div>
+                <div class="wc-p-msg">
+                  <span>Provided by ATK Networks.</span>
+                  <span>회원가입</span>
+                </div>
+              </div>
+            </div>
+            <div class="wc-advertising wc-sb-advertising snx">
+              <span>AD</span>
+            </div>
+            <div class="wc-keywords snx wc-block">
+              <div class="wc-title">
+                <span>인기 검색어</span>
+              </div>
+              <div class="wc-keywords-view">
+                <div class="wc-keywords-view-all">
+                  <ul>
+                    <li>
+                      <span>1</span>
+                      <a href="/search?q=네이버 웨일">Alan Walker</a>
+                    </li>
+                    <li>
+                      <span>2</span>
+                      <a href="/search?q=Marshmello">Da Tweekaz</a>
+                    </li>
+                    <li>
+                      <span>3</span>
+                      <a href="/search?q=Twice">Twice</a>
+                    </li>
+                    <li>
+                      <span>4</span>
+                      <a href="/search?q=Ava Max">Ava Max</a>
+                    </li>
+                    <li>
+                      <span>5</span>
+                      <a href="/search?q=Afrojack">Afrojack</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer"></div>
+    </div>
+  </body>
+</html>
